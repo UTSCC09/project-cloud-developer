6bf6afac4212a21659dee3110e974e99decb07bf
Andrew D'Amario <andrew@thecross.ca>
2022-07-13 02:43:39 -0400

    Format code.

--- slacker-tracker/src/components/Sign/login.js
+++ slacker-tracker/src/components/Sign/login.js
@@ -1,41 +1,40 @@
-import React from 'react'
-import { GoogleLogin} from 'react-google-login';
-import { useEffect } from 'react';
-import { gapi } from 'gapi-script';
+import React, { useEffect } from 'react'
+import { GoogleLogin } from 'react-google-login'
+import { gapi } from 'gapi-script'
 
-export default function Login() {
+export default function Login () {
+  const clientId =
+    '131011506414-9hmdp9c3ve0dvun0c3otqpgpovdd2fh9.apps.googleusercontent.com'
 
-    const clientId = "131011506414-9hmdp9c3ve0dvun0c3otqpgpovdd2fh9.apps.googleusercontent.com";
-
-    useEffect(() => {
-        function start() {
-            gapi.client.init({
-                clientId: clientId,
-                scope: "email profile"
-            })
-        }
-
-        gapi.load('client:auth2', start);
-    })
-
-    const onSuccess = res => {
-        console.log("LOGIN SUCCESS! Current user: ", res.profileObj);
+  useEffect(() => {
+    function start () {
+      gapi.client.init({
+        clientId,
+        scope: 'email profile'
+      })
     }
 
-    const onFailure = res => {
-        console.log("LOGIN FAILED! Current user: ", res);
-    }
-    
-    return (
-        <div id="google-signin-button">
-            <GoogleLogin
-                clientId={clientId}
-                buttonText="Login"
-                onSuccess={onSuccess}
-                onFailure={onFailure}
-                cookiePolicy={'single_host_origin'}
-                isSignedIn={true}
-            />
-        </div>
-    )
+    gapi.load('client:auth2', start)
+  })
+
+  const onSuccess = (res) => {
+    console.log('LOGIN SUCCESS! Current user: ', res.profileObj)
+  }
+
+  const onFailure = (res) => {
+    console.log('LOGIN FAILED! Current user: ', res)
+  }
+
+  return (
+    <div id="google-signin-button">
+      <GoogleLogin
+        clientId={clientId}
+        buttonText="Login"
+        onSuccess={onSuccess}
+        onFailure={onFailure}
+        cookiePolicy={'single_host_origin'}
+        isSignedIn={true}
+      />
+    </div>
+  )
 }
--- slacker-tracker/src/components/Sign/logout.js
+++ slacker-tracker/src/components/Sign/logout.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import { GoogleLogout } from 'react-google-login';
+import React from 'react'
+import { GoogleLogout } from 'react-google-login'
 
-export default function logout() {
-
-  const clientId = "131011506414-9hmdp9c3ve0dvun0c3otqpgpovdd2fh9.apps.googleusercontent.com";
+export default function logout () {
+  const clientId =
+    '131011506414-9hmdp9c3ve0dvun0c3otqpgpovdd2fh9.apps.googleusercontent.com'
 
   const onSuccess = () => {
-    console.log("LOGOUT SUCCESS!");
+    console.log('LOGOUT SUCCESS!')
   }
 
   return (
     <div id="google-signin-button">
-        <GoogleLogout
-            clientId={clientId}
-            buttonText="Logout"
-            onLogoutSuccess={onSuccess}
-        />
+      <GoogleLogout
+        clientId={clientId}
+        buttonText="Logout"
+        onLogoutSuccess={onSuccess}
+      />
     </div>
   )
 }
