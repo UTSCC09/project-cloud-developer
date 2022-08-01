344c821935a8cbc50c9378cb0c1d38b0275708b6
Andrew D'Amario <51063733+andrewaclear@users.noreply.github.com>
2022-08-01 04:23:53 -0400

    Format documents.

--- slacker-tracker/mailer.js
+++ slacker-tracker/mailer.js
@@ -1,78 +1,78 @@
-const nodemailer = require("nodemailer");
-const { google } = require("googleapis");
-const OAuth2 = google.auth.OAuth2;
-const { workerData, parentPort } = require("worker_threads");
-require("dotenv").config();
+const nodemailer = require('nodemailer')
+const { google } = require('googleapis')
+const OAuth2 = google.auth.OAuth2
+const { workerData, parentPort } = require('worker_threads')
+require('dotenv').config()
 
 const convertMsToHM = (milliseconds) => {
-  let seconds = Math.floor(milliseconds / 1000);
-  let minutes = Math.floor(seconds / 60);
-  let hours = Math.floor(minutes / 60);
+  let seconds = Math.floor(milliseconds / 1000)
+  let minutes = Math.floor(seconds / 60)
+  let hours = Math.floor(minutes / 60)
 
-  seconds = seconds % 60;
-  minutes = seconds >= 30 ? minutes + 1 : minutes;
-  minutes = minutes % 60;
-  hours = hours % 24;
+  seconds = seconds % 60
+  minutes = seconds >= 30 ? minutes + 1 : minutes
+  minutes = minutes % 60
+  hours = hours % 24
 
-  return `${hours.toString().padStart(2, "0")} hours ${minutes
+  return `${hours.toString().padStart(2, '0')} hours ${minutes
     .toString()
-    .padStart(2, "0")} minutes`;
-};
+    .padStart(2, '0')} minutes`
+}
 
 const calculatePercentage = function (friendScores, userScore) {
-  const friendBelow = friendScores.filter((score) => score <= userScore);
-  return Math.round(friendBelow.length / friendScores.length) * 100;
-};
+  const friendBelow = friendScores.filter((score) => score <= userScore)
+  return Math.round(friendBelow.length / friendScores.length) * 100
+}
 
 const getAvg = function (friendScores, userScore) {
-  const sum = friendScores.reduce((arr, cur) => arr + cur) + userScore;
-  return Math.round(sum / (friendScores.length + 1));
-};
+  const sum = friendScores.reduce((arr, cur) => arr + cur) + userScore
+  return Math.round(sum / (friendScores.length + 1))
+}
 
 const createTransporter = async () => {
   const oauth2Client = new OAuth2(
     process.env.CLIENT_ID,
     process.env.CLIENT_SECRET,
-    "https://developers.google.com/oauthplayground"
-  );
+    'https://developers.google.com/oauthplayground'
+  )
 
   oauth2Client.setCredentials({
-    refresh_token: process.env.REFRESH_TOKEN,
-  });
+    refresh_token: process.env.REFRESH_TOKEN
+  })
 
   const accessToken = await new Promise((resolve, reject) => {
     oauth2Client.getAccessToken((err, token) => {
       if (err) {
-        reject("Failed to create access token :(");
+        reject('Failed to create access token :(')
       }
-      resolve(token);
-    });
-  });
+      resolve(token)
+    })
+  })
 
   const transport = nodemailer.createTransport({
-    service: "gmail",
+    service: 'gmail',
     auth: {
-      type: "OAuth2",
+      type: 'OAuth2',
       accessToken,
       clientId: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
       refreshToken: process.env.REFRESH_TOKEN,
-      user: process.env.EMAIL_USERNAME,
+      user: process.env.EMAIL_USERNAME
     },
     tls: {
-      rejectUnauthorized: false,
-    },
-  });
+      rejectUnauthorized: false
+    }
+  })
 
-  return transport;
-};
+  return transport
+}
 
 const sendEmail = async (emailOptions) => {
-  let emailTransporter = await createTransporter();
-  await emailTransporter.sendMail(emailOptions);
-};
+  const emailTransporter = await createTransporter()
+  await emailTransporter.sendMail(emailOptions)
+}
 
-console.log("workerdata", workerData);
+console.log('workerdata', workerData)
 const sendHTML = `
 <div>
   <includetail>
@@ -180,12 +180,12 @@ const sendHTML = `
         </tbody>
     </table>
   </includetail>
-</div>`;
+</div>`
 
 sendEmail({
-  subject: "Slacker Tracker Daily Summary",
+  subject: 'Slacker Tracker Daily Summary',
   html: sendHTML,
-  to: workerData.sendEmail,
-});
+  to: workerData.sendEmail
+})
 
-parentPort.postMessage({ workerData });
+parentPort.postMessage({ workerData })
